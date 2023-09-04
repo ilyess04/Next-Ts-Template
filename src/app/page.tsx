39c1fdd251860f8df268990cd693214f2cfca709
Sellami Ilyess <dev@ilyessellami.com>
2023-09-04 16:54:42 +0200

    Init app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
 "use client";
 import { HomeTemplate } from "@/templates";
-import useLocales from "@/common/hooks/useLocales/useLocales";
 
 const Home = (): JSX.Element => {
-  const locales = useLocales();
-  console.log(locales, "xxxxxxxxxx");
   return <HomeTemplate />;
 };
 export default Home;
